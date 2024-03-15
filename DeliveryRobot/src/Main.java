4c25d9f1cc0a97145103ab0e9624bc44f3c264e8
Maksim Popov <maksim2popov@gmail.com>
2024-03-15 21:52:51 +0300

    Create freqlog

--- DeliveryRobot/src/Main.java
+++ DeliveryRobot/src/Main.java
@@ -1,16 +1,13 @@
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
 
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        // Thread's logic
-        Runnable logic = () -> {
+        // Counting thread's logic
+        Runnable counting = () -> {
             String route = generateRoute("RLRFR", 100);
             int count = 0;
 
@@ -24,25 +21,48 @@ public class Main {
                 } else {
                     sizeToFreq.put(count, 1);
                 }
+
+                sizeToFreq.notify();
             }
         };
 
-        // Threads' creation
+        // Freqlog thread's logic
+        Runnable logging = () -> {
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    // Finding key for max freq
+                    int maxKey = Collections.max(sizeToFreq.entrySet(), Map.Entry.comparingByValue()).getKey();
+                    System.out.printf("Самое частое количество повторений %d (встретилось %d раз) \n", maxKey, sizeToFreq.get(maxKey));
+                }
+            }
+        };
+
+        List<Thread> threads = new ArrayList<>();
+
+        // Logging thread's creation
+        Thread freqLog = new Thread(logging);
+        freqLog.start();
+
+        // Counting threads' creation
         for (int i = 0; i < 1000; i++) {
-            Thread thread = new Thread(logic);
+            Thread thread = new Thread(counting);
+            threads.add(thread);
             thread.start();
         }
 
-        // Finding key for max freq
-        int maxKey = Collections.max(sizeToFreq.entrySet(), Map.Entry.comparingByValue()).getKey();
-
-        // Summary
-        System.out.printf("Самое частое количество повторений %d (встретилось %d раз) \n", maxKey, sizeToFreq.get(maxKey));
-        System.out.println("Другие размеры: ");
-
-        for (Integer size : sizeToFreq.keySet()) {
-            System.out.printf("- %d (%d раз) \n", size, sizeToFreq.get(size));
+        // FreqLog thread's interruption
+        for (Thread thread : threads) {
+            thread.join();
         }
+        freqLog.interrupt();
+
 
     }
 
